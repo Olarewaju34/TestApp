57e51ced7457681dae4bc6fe87fd6ffbb4ca78a8
Olarewaju34 <mohmzyo@gmail.com>
2022-06-18 11:43:46 +0100

    TestApp

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more informati
+Console.WriteLine("enter first name");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("enter last name");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("enter age");
+int age =Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("enter favourite colour");
+string favouritecolour = Console.ReadLine();
+
+Console.WriteLine("enter  favourite food");
+string favouritefood=Console.ReadLine();
+
+Console.WriteLine("origin");
+string origin = Console.ReadLine();
+ 
+ 
+ 
+//interpolation
+string interpolation=$"my name is {firstName} {lastName} I am {age} years old.\n my favourite colour is {favouritecolour} and my favourite food is {favouritefood}";
+Console.WriteLine(interpolation);
+
+Console.ReadKey();
+
+
+
+
+
+
